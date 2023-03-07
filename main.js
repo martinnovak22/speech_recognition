fd394665c88556052ba41f705d5ad4065d29d4f9
Martin Novák <martin.novak@argo22.com>
2023-03-07 18:10:43 +0100

    Updated landing page
    - update for landing page

--- main.js
+++ main.js
@@ -1,23 +1,48 @@
-if (e.results[0].isFinal) {
-  if (text.includes("how are you")) {
+const texts = document.querySelector(".texts");
+const startButton = document.querySelector("#start");
+
+let p = document.createElement("p");
+
+window.SpeechRecognition =
+  window.SpeechRecognition || window.webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+recognition.interimResults = true;
+
+startButton.addEventListener("click", () => {
+  recognition.start();
+  console.log("start");
+});
+
+recognition.addEventListener("result", (e) => {
+  texts.appendChild(p);
+  const text = Array.from(e.results)
+    .map((result) => result[0])
+    .map((result) => result.transcript)
+    .join("");
+
+  p.innerText = text;
+
+  if (e.results[0].isFinal) {
+    if (text.includes("how are you")) {
+      p = document.createElement("p");
+      p.classList.add("replay");
+      p.innerText = "I am fine";
+      texts.appendChild(p);
+    }
+    if (
+      text.includes("What's your name") ||
+      text.includes("what is your name")
+    ) {
+      p = document.createElement("p");
+      p.innerText = "My Name is Martin";
+      texts.appendChild(p);
+    }
+    if (text.includes("Open YouTube")) {
+      p = document.createElement("p");
+      texts.appendChild(p);
+      window.open("https://www.youtube.com/");
+    }
     p = document.createElement("p");
-    p.classList.add("replay");
-    p.innerText = "I am fine";
-    texts.appendChild(p);
   }
-  if (text.includes("what's your name") || text.includes("what is your name")) {
-    p = document.createElement("p");
-    p.classList.add("replay");
-    p.innerText = "My Name is Cifar";
-    texts.appendChild(p);
-  }
-  if (text.includes("open my YouTube")) {
-    p = document.createElement("p");
-    p.classList.add("replay");
-    p.innerText = "opening youtube channel";
-    texts.appendChild(p);
-    console.log("opening youtube");
-    window.open("https://www.youtube.com/channel/UCdxaLo9ALJgXgOUDURRPGiQ");
-  }
-  p = document.createElement("p");
-}
+});
