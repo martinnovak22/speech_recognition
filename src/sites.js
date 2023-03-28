5a0860be7d3699396a3412cc7150394eeb5d173e
Martin Novák <martin.novak@argo22.com>
2023-03-28 10:37:50 +0200

    Link-fix
    - seznam.cz

--- src/sites.js
+++ src/sites.js
@@ -2,5 +2,5 @@ export const default_sites_list = [
   { name: "youtube", url: "http://www.youtube.com" },
   { name: "google", url: "http://www.google.com" },
   { name: "facebook", url: "http://www.facebook.com" },
-  { name: "seznam", url: "http://www.seznam.com" },
+  { name: "seznam", url: "http://www.seznam.cz" },
 ];
